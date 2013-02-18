41a30b085644cae70db53ffe5bebeee02d4f2417
Andrea Guarise <andrea.guarise@to.infn.it>
2013-02-18 11:02:26 +0100

    Fixed errors

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,3 +1,5 @@
 class Resource < ActiveRecord::Base
   attr_accessible :description, :name, :resource_type_id, :site_id
+  belongs_to :ResourceType
+  belongs_to :Site
 end
--- app/models/resource_type.rb
+++ app/models/resource_type.rb
@@ -1,3 +1,4 @@
 class ResourceType < ActiveRecord::Base
   attr_accessible :name
+  #has_many :resources, :dependent => :destroy
 end
