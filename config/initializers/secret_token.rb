403d67dbc6b057b0ea2c6c5bcbfe996b39603519
Andrea Guarise <andrea.guarise@to.infn.it>
2013-02-18 10:26:14 +0100

    Added Site scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AccountingService::Application.config.secret_token = '2c9dbf511ca5872c69eeac41688fdbaf37928b7cec002db2213a3aed5db22c5937a91a65ba9d8795f7d0e3e354f734452a633062ebe15c3f78572e48c468f4b3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AccountingService::Application.config.session_store :cookie_store, :key => '_accountingService_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AccountingService::Application.config.session_store :active_record_store
