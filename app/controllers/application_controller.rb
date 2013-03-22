b734a12bfeafd4dbe866608d56d6ca6b43c93d4c
Andrea Guarise <andrea.guarise@to.infn.it>
2013-03-22 16:42:03 +0100

    check for recor uniqueness

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   protected
-
   def userAuthenticate
-     unless User.find_by_id(session[:user_id])
-       redirect_to login_url, :notice => "Please log in"
-     end
+    unless User.find_by_id(session[:user_id])
+      redirect_to login_url, :notice => "Please log in"
+    end
   end
-  
+
   def publisherAuthenticate
-    authenticate_or_request_with_http_token do |token, options|
-      ip = request.remote_ip
-      Publisher.authenticate(ip,token)
+    unless session[:user_id]
+
+      authenticate_or_request_with_http_token do |token, options|
+        ip = request.remote_ip
+        Publisher.authenticate(ip,token)
+      end
     end
   end
 end
