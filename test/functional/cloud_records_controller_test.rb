d86c647d070b88d2371967b97957a9130dd6312f
Andrea Guarise <andrea.guarise@to.infn.it>
2013-03-26 14:39:49 +0100

    Fixed functional test to consider VMUUID uinqueness

--- test/functional/cloud_records_controller_test.rb
+++ test/functional/cloud_records_controller_test.rb
@@ -6,6 +6,8 @@ class CloudRecordsControllerTest < ActionController::TestCase
     request.env['HTTP_AUTHORIZATION'] =  ActionController::HttpAuthentication::Token.encode_credentials("1238.1238")
     @cloud_record = cloud_records(:one)
     @resource = resources(:one)
+    @cloud_record_new = cloud_records(:two)
+    @cloud_record_new.VMUUID = "abcdefghilmn"
   end
 
   test "should get index" do
@@ -21,7 +23,7 @@ class CloudRecordsControllerTest < ActionController::TestCase
 
   test "should create cloud_record" do
     assert_difference('CloudRecord.count') do
-      post :create, :cloud_record => { :FQAN => @cloud_record.FQAN, :VMUUID => @cloud_record.VMUUID, :cloudType => @cloud_record.cloudType, :cpuCount => @cloud_record.cpuCount, :cpuDuration => @cloud_record.cpuDuration, :disk => @cloud_record.disk, :diskImage => @cloud_record.diskImage, :endTime => @cloud_record.endTime, :globaluserName => @cloud_record.globaluserName, :localVMID => @cloud_record.localVMID, :local_group => @cloud_record.local_group, :local_user => @cloud_record.local_user, :memory => @cloud_record.memory, :networkInbound => @cloud_record.networkInbound, :networkOutBound => @cloud_record.networkOutBound, :networkType => @cloud_record.networkType, :startTime => @cloud_record.startTime, :status => @cloud_record.status, :storageRecordId => @cloud_record.storageRecordId, :suspendDuration => @cloud_record.suspendDuration, :wallDuration => @cloud_record.wallDuration }, :resource_name => @resource.name    
+      post :create, :cloud_record => { :FQAN => @cloud_record_new.FQAN, :VMUUID => @cloud_record_new.VMUUID, :cloudType => @cloud_record_new.cloudType, :cpuCount => @cloud_record_new.cpuCount, :cpuDuration => @cloud_record_new.cpuDuration, :disk => @cloud_record_new.disk, :diskImage => @cloud_record_new.diskImage, :endTime => @cloud_record_new.endTime, :globaluserName => @cloud_record_new.globaluserName, :localVMID => @cloud_record_new.localVMID, :local_group => @cloud_record_new.local_group, :local_user => @cloud_record_new.local_user, :memory => @cloud_record_new.memory, :networkInbound => @cloud_record_new.networkInbound, :networkOutBound => @cloud_record_new.networkOutBound, :networkType => @cloud_record_new.networkType, :startTime => @cloud_record_new.startTime, :status => @cloud_record_new.status, :storageRecordId => @cloud_record_new.storageRecordId, :suspendDuration => @cloud_record_new.suspendDuration, :wallDuration => @cloud_record_new.wallDuration }, :resource_name => @resource.name    
     end
 
     assert_redirected_to cloud_record_path(assigns(:cloud_record))
