bde551747104c7131ae0505e1193cfe5c8276a60
Andrea Guarise <andrea.guarise@to.infn.it>
2013-04-04 11:37:27 +0200

    modified navigation bar to differentiate among admin and plain users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
   def create
     if user = User.authenticate(params[:name], params[:password])
       session[:user_id] = user.id
+      session[:role] = user.role.name
       redirect_to main_url
     else
       redirect_to login_url, :alert => "Invalid user/password combination"
@@ -14,6 +15,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
+    session[:role] = nil
     redirect_to main_url, :notice => "Logged out"
   end
 end
