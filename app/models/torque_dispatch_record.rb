93e58d6c2aa470cadb376ef382a1176b28c0ec84
Andrea Guarise <andrea.guarise@to.infn.it>
2013-04-10 11:03:08 +0200

    Addedd validation for presence of publisher id

--- app/models/torque_dispatch_record.rb
+++ app/models/torque_dispatch_record.rb
@@ -1,4 +1,5 @@
 class TorqueDispatchRecord < ActiveRecord::Base
   attr_accessible :lrmsId, :recordDate, :requestor, :uniqueId, :publisher_id
+  validates :publisher_id, :presence => true, :on => :create
   belongs_to :publisher
 end
