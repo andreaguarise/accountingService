4956eca7330765c3fe20108ccd9bb971581b011f
Andrea Guarise <andrea.guarise@to.infn.it>
2013-04-11 16:18:32 +0200

    cloud_record now gets publisher by token. subsequently gets resource and site by object associations

--- app/models/cloud_record.rb
+++ app/models/cloud_record.rb
@@ -1,10 +1,8 @@
 class CloudRecord < ActiveRecord::Base
   attr_accessible :FQAN, :VMUUID, :cloudType, :cpuCount, :cpuDuration, :disk, :diskImage, :endTime, :globaluserName, :localVMID, :local_group, :local_user, :memory, :networkInbound, :networkOutBound, :networkType, :publisher_id, :resource_id, :startTime, :status, :storageRecordId, :suspendDuration, :wallDuration
   belongs_to :publisher
-  belongs_to :resource #FIXME association should be removed since this will go through publisher->resource
   validates :VMUUID, :presence => true, :uniqueness => true, :on => :create
-  validates :resource_id, :presence => true, :on => :create
   validates :publisher_id, :presence => true, :on => :create
   delegate :resource, :to => :publisher
-  delegate :site, :to => :resource #FIXME this should go through the delegate :resource, check.
+  delegate :site, :to => :resource
 end
