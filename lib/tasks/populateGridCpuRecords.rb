a0b25962cb804ba5da6403b38360d3efeac3705b
Andrea Guarise <andrea.guarise@to.infn.it>
2013-05-16 16:51:17 +0200

    Fixed logi to start from last converted record

--- lib/tasks/populateGridCpuRecords.rb
+++ lib/tasks/populateGridCpuRecords.rb
@@ -1,4 +1,10 @@
-BlahRecord.find_each(:batch_size => 50) { |b|
+lastInsertedGridRecord = GridCpuRecord.last
+lastInsertedBlahRecordId = 0
+if lastInsertedGridRecord
+  lastInsertedBlahRecordId = lastInsertedGridRecord.blah_record_id 
+end
+
+BlahRecord.find_each(:batch_size => 50, :start => lastInsertedBlahRecordId ) { |b|
   t = TorqueExecuteRecord.where("lrmsId=? AND recordDate >= ?",b.lrmsId,b.recordDate)
   if t[0]
     g= GridCpuRecord.new()
