ea84a7501464cc977f1ec2a0ad0bc71861fb6d4f
Andrea Guarise <andrea.guarise@to.infn.it>
2013-05-22 17:18:09 +0200

    fixed limit

--- db/migrate/20130522150314_change_column_to_emi_storage_records.rb
+++ db/migrate/20130522150314_change_column_to_emi_storage_records.rb
@@ -1,9 +1,9 @@
 class ChangeColumnToEmiStorageRecords < ActiveRecord::Migration
   def up
-    change_column :emi_storage_records,:resourceCapacityUsed, :integer, :limit => 20
-    change_column :emi_storage_records,:resourceCapacityAllocated, :integer, :limit => 20
-    change_column :emi_storage_records,:logicalCapacityUsed, :integer, :limit => 20
-    change_column :emi_storage_records,:fileCount, :integer, :limit => 20
+    change_column :emi_storage_records,:resourceCapacityUsed, :integer, :limit => 8
+    change_column :emi_storage_records,:resourceCapacityAllocated, :integer, :limit => 8
+    change_column :emi_storage_records,:logicalCapacityUsed, :integer, :limit => 8
+    change_column :emi_storage_records,:fileCount, :integer, :limit => 8
   end
 
   def down
