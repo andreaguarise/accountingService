c84d8edc02c412bedbeac9c070a32a73945c67be
Andrea Guarise <andrea.guarise@to.infn.it>
2013-05-28 14:40:08 +0200

    Deleted files

--- gridCpuRecordScaffld.sh
+++
@@ -1,4 +0,0 @@
-rails generate scaffold GridCpuRecord \
-recordlike_id:integer \
-recordlike_type:string \
-blah_record_id:integer
--- populateGridCpuRecords.rb
+++
@@ -1,9 +0,0 @@
-BlahRecord.find_each(:batch_size => 10) { |b|
-  t = BatchExecuteRecord.where("lrmsId=? AND recordDate > ?",b.lrmsId,b.recordDate)
-  if t[0]
-    g= GridCpuRecord.new()
-    g.blah_record_id = b.id
-    g.batch_execute_record_id = t[0].id
-    g.save
-  end
-}
