9dd0bfc49444401601e80c4edf4c8cfe260cfee8
Andrea Guarise <andrea.guarise@to.infn.it>
2013-05-28 16:51:13 +0200

    Torque to Batch

--- lib/tasks/populateGridCpuRecords.rb
+++ lib/tasks/populateGridCpuRecords.rb
@@ -5,7 +5,7 @@ if lastInsertedGridRecord
 end
 
 BlahRecord.find_each(:batch_size => 50, :start => lastInsertedBlahRecordId ) { |b|
-  t = TorqueExecuteRecord.where("lrmsId=? AND recordDate >= ?",b.lrmsId,b.recordDate)
+  t = BatchExecuteRecord.where("lrmsId=? AND recordDate >= ?",b.lrmsId,b.recordDate)
   if t[0]
     g= GridCpuRecord.new()
     g.blah_record_id = b.id
