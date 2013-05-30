da152e9a8b0d52268720a786551d11c9e4faf19b
Andrea Guarise <andrea.guarise@to.infn.it>
2013-05-30 14:20:50 +0200

    Fixed error on SQL statement when backend is MYSQL

--- app/models/local_cpu_record.rb
+++ app/models/local_cpu_record.rb
@@ -3,7 +3,7 @@
 
 class LocalCpuRecord < BatchExecuteRecord
   def self.all
-    BatchExecuteRecord.find_by_sql("SELECT \"batch_execute_records\".* FROM batch_execute_records LEFT JOIN grid_cpu_records ON grid_cpu_records.batch_execute_record_id=batch_execute_records.id WHERE grid_cpu_records.id IS NULL")
+    BatchExecuteRecord.find_by_sql("SELECT batch_execute_records.* FROM batch_execute_records LEFT JOIN grid_cpu_records ON grid_cpu_records.batch_execute_record_id=batch_execute_records.id WHERE grid_cpu_records.id IS NULL")
   end
 
 end
