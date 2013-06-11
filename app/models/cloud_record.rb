642d068354a998ff1e527b40df2256a1accdf974
Andrea Guarise <andrea.guarise@to.infn.it>
2013-06-11 17:08:14 +0200

    startTime and endTime can be translated from unixtime

--- app/models/cloud_record.rb
+++ app/models/cloud_record.rb
@@ -1,8 +1,25 @@
 class CloudRecord < ActiveRecord::Base
   attr_accessible :FQAN, :VMUUID, :cloudType, :cpuCount, :cpuDuration, :disk, :diskImage, :endTime, :globaluserName, :localVMID, :local_group, :local_user, :memory, :networkInbound, :networkOutBound, :networkType, :publisher_id, :resource_id, :startTime, :status, :storageRecordId, :suspendDuration, :wallDuration
+  before_create :translateDates
   belongs_to :publisher
   validates :VMUUID, :presence => true, :uniqueness => true, :on => :create
   validates :publisher_id, :presence => true, :on => :create
   delegate :resource, :to => :publisher
   delegate :site, :to => :resource
+  
+  protected
+
+  def translateDates
+    logger.info "translateDates, startTime:#{self.startTime_before_type_cast}, endTime:#{self.attributes["endTime"]}"
+    if /[[:digit:]]{10}/.match(self.endTime_before_type_cast.to_s) 
+      logger.info "endTime:#{self.endTime_before_type_cast.to_s} --> "
+      self.endTime = Time.at(self.endTime_before_type_cast.to_i)
+    end
+    if /[[:digit:]]{10}/.match(self.startTime_before_type_cast.to_s)
+      logger.info "startTime:#{self.startTime_before_type_cast.to_s} --> " 
+      self.startTime = Time.at(self.startTime_before_type_cast.to_i)
+    end
+    #self.uniqueId = self.recordDate.to_s + "-" + self.lrmsId
+  end
+  
 end
