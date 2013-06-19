b3f100cbf225fb335973c0460baa706cb92ad113
Andrea Guarise <andrea.guarise@to.infn.it>
2013-06-19 16:17:30 +0200

    Added auth to test controller

--- test/functional/storage_summaries_controller_test.rb
+++ test/functional/storage_summaries_controller_test.rb
@@ -2,6 +2,8 @@ require 'test_helper'
 
 class StorageSummariesControllerTest < ActionController::TestCase
   setup do
+    @request.env['REMOTE_ADDR'] = '1.2.3.4'
+    request.env['HTTP_AUTHORIZATION'] =  ActionController::HttpAuthentication::Token.encode_credentials("1238.1238")
     @storage_summary = storage_summaries(:one)
   end
 
