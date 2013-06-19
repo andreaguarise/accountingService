5dafa580b605bdcada1df10b49ca36b5332e1153
Andrea Guarise <andrea.guarise@to.infn.it>
2013-06-19 14:51:20 +0200

    Improved view. Implemente xml

--- app/controllers/benchmark_types_controller.rb
+++ app/controllers/benchmark_types_controller.rb
@@ -2,34 +2,46 @@ class BenchmarkTypesController < ApplicationController
   skip_before_filter :userAuthenticate
   # GET /benchmark_types
   # GET /benchmark_types.json
+  # GET /benchmark_types.xml
   def index
-    @benchmark_types = BenchmarkType.all
+    @benchmark_types = BenchmarkType.search(params[:key],params[:search])
+    respond_to do |format|
+      format.html {
+        @benchmark_types = tableSort!(@benchmark_types, params, "name")
+    }
+    format.any(:xml,:json) {}
+    end
 
     respond_to do |format|
       format.html # index.html.erb
       format.json { render :json => @benchmark_types }
+      format.xml { render :xml => @benchmark_types }
     end
   end
 
   # GET /benchmark_types/1
   # GET /benchmark_types/1.json
+  # GET /benchmark_types/1.xml
   def show
     @benchmark_type = BenchmarkType.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
       format.json { render :json => @benchmark_type }
+      format.xml { render :xml => @benchmark_type }
     end
   end
 
   # GET /benchmark_types/new
   # GET /benchmark_types/new.json
+  # GET /benchmark_types/new.xml
   def new
     @benchmark_type = BenchmarkType.new
 
     respond_to do |format|
       format.html # new.html.erb
       format.json { render :json => @benchmark_type }
+      format.xml { render :xml => @benchmark_type }
     end
   end
 
@@ -40,6 +52,7 @@ class BenchmarkTypesController < ApplicationController
 
   # POST /benchmark_types
   # POST /benchmark_types.json
+  # POST /benchmark_types.xml
   def create
     @benchmark_type = BenchmarkType.new(params[:benchmark_type])
 
@@ -47,15 +60,18 @@ class BenchmarkTypesController < ApplicationController
       if @benchmark_type.save
         format.html { redirect_to @benchmark_type, :notice => 'Benchmark type was successfully created.' }
         format.json { render :json => @benchmark_type, :status => :created, :location => @benchmark_type }
+        format.xml { render :xml => @benchmark_type, :status => :created, :location => @benchmark_type }
       else
         format.html { render :action => "new" }
         format.json { render :json => @benchmark_type.errors, :status => :unprocessable_entity }
+        format.xml { render :xml => @benchmark_type.errors, :status => :unprocessable_entity }
       end
     end
   end
 
   # PUT /benchmark_types/1
   # PUT /benchmark_types/1.json
+  # PUT /benchmark_types/1.xml
   def update
     @benchmark_type = BenchmarkType.find(params[:id])
 
@@ -63,15 +79,18 @@ class BenchmarkTypesController < ApplicationController
       if @benchmark_type.update_attributes(params[:benchmark_type])
         format.html { redirect_to @benchmark_type, :notice => 'Benchmark type was successfully updated.' }
         format.json { head :no_content }
+        format.xml { head :no_content }
       else
         format.html { render :action => "edit" }
         format.json { render :json => @benchmark_type.errors, :status => :unprocessable_entity }
+        format.xml { render :xml => @benchmark_type.errors, :status => :unprocessable_entity }
       end
     end
   end
 
   # DELETE /benchmark_types/1
   # DELETE /benchmark_types/1.json
+  # DELETE /benchmark_types/1.xml
   def destroy
     @benchmark_type = BenchmarkType.find(params[:id])
     @benchmark_type.destroy
@@ -79,6 +98,7 @@ class BenchmarkTypesController < ApplicationController
     respond_to do |format|
       format.html { redirect_to benchmark_types_url }
       format.json { head :no_content }
+      format.xml { head :no_content }
     end
   end
 end
--- app/models/benchmark_type.rb
+++ app/models/benchmark_type.rb
@@ -2,4 +2,5 @@ class BenchmarkType < ActiveRecord::Base
   include Search
   attr_accessible :description, :name
   has_many :benchmark_values
+  
 end
