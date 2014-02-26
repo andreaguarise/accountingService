43613a017ef8bff5bdca50667ee3a7136b49a40d
Andrea Guarise <andrea.guarise@to.infn.it>
2014-02-26 14:35:59 +0100

    Added search and graph functionalities to cloud record summaries

--- app/controllers/cloud_record_summaries_controller.rb
+++ app/controllers/cloud_record_summaries_controller.rb
@@ -31,13 +31,52 @@ class CloudRecordSummariesController < ApplicationController
 
   # GET /cloud_record_summaries/search?VMUUID=string
   def search
-    respond_to do |format|
-      format.html {
-        @cloud_record_summaries = CloudRecordSummary.paginate(:page=>params[:page], :per_page => config.itemsPerPageHTML).all
-      }
-      format.any(:xml,:json){
-        @cloud_record_summaries = CloudRecordSummary.paginate(:page=>params[:page], :per_page => config.itemsPerPage).all
-      }
+    whereBuffer = String.new
+    groupBuffer = String.new
+    logger.info "Entering #search method."
+    
+    params.each do |param_k,param_v|
+      if CloudRecordSummary.attrSearchable.include?(param_k) && param_v != ""
+        logger.info "Got search parameter: #{param_k} ==> #{param_v}" 
+        if ( whereBuffer != "" )
+          whereBuffer += " AND "
+        end
+        whereBuffer += "#{param_k}=\"#{param_v}\"" 
+      end
+   end
+    
+    relationBuffer = "date as ordered_date,sum(wallDuration/60) as wall,count(*) as count,sum(networkInbound/1048576) as netIn,sum(networkOutBound/1048576) as netOut"
+    groupBuffer = "ordered_date"
+    @cloud_record_summaries = CloudRecordSummary.paginate(:page=>params[:page], :per_page => 2).orderByParms('id desc',params).where(whereBuffer).all
+    if params[:doGraph] == "1"
+      min_max =  CloudRecordSummary.select("min(date) as minDate,max(date) as maxDate").where(whereBuffer)
+      graph_ary = CloudRecordSummary.select(relationBuffer).where(whereBuffer).group(groupBuffer).order(:ordered_date)
+      tableCpu = GoogleVisualr::DataTable.new
+      tableCount = GoogleVisualr::DataTable.new
+      tableNet = GoogleVisualr::DataTable.new
+    
+      # Add Column Headers
+      tableCpu.new_column('date', 'Date' )
+      tableCpu.new_column('number', 'wall time (min)')
+      
+      tableCount.new_column('date', 'Date' )
+      tableCount.new_column('number', 'count')
+    
+      tableNet.new_column('date', 'Date' )
+      tableNet.new_column('number', 'inbound net (MB)')
+      tableNet.new_column('number', 'outbound net (MB)')
+    
+      graph_ary.each do |row|
+        tableCpu.add_row([row.ordered_date.to_datetime,row.wall.to_i])
+        tableCount.add_row([row.ordered_date.to_datetime,row.count.to_i])
+        tableNet.add_row([row.ordered_date.to_datetime,row.netIn.to_i,row.netOut.to_i])
+      end 
+      optionCpu = { :width => 1100, :height => 215, :title => 'VM CPU History', :hAxis => {:minValue => min_max.first.minDate.to_datetime, :maxValue => min_max.first.maxDate.to_datetime} }
+      @chartCpu = GoogleVisualr::Interactive::AreaChart.new(tableCpu, optionCpu)
+      optionCount = { :width => 1100, :height => 215, :title => 'VM Count', :hAxis => {:minValue => min_max.first.minDate.to_datetime, :maxValue => min_max.first.maxDate.to_datetime} }
+      @chartCount = GoogleVisualr::Interactive::AreaChart.new(tableCount, optionCount)
+      optionNet = { :width => 1100, :height => 215, :title => 'VM Network History', :hAxis => {:minValue => min_max.first.minDate.to_datetime, :maxValue => min_max.first.maxDate.to_datetime} }
+      @chartNet = GoogleVisualr::Interactive::AreaChart.new(tableNet, optionNet)
     end
     respond_to do |format|
       format.html # index.html.erb
--- app/models/cloud_record_summary.rb
+++ app/models/cloud_record_summary.rb
@@ -1,4 +1,5 @@
 class CloudRecordSummary < ActiveRecord::Base
+  include Search
   attr_accessible :cpuCount, :date, :local_group, :local_user, :memory, :networkInBound, :networkOutBound, :site_id, :vmCount, :wallDuration
   validates_uniqueness_of :date, :scope =>[:site_id,:local_group,:local_user]
   belongs_to :site
