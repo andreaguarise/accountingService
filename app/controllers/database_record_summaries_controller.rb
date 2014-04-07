d1d6bb32f52c793a22bb719f13afc675e1c94e4c
Andrea Guarise <andrea.guarise@to.infn.it>
2014-04-07 11:21:11 +0200

    Fixed error in scaffold generation

--- app/controllers/database_record_summaries_controller.rb
+++ app/controllers/database_record_summaries_controller.rb
@@ -1,4 +1,5 @@
 class DatabaseRecordSummariesController < ApplicationController
+  skip_before_filter :userAuthenticate
   # GET /database_record_summaries
   # GET /database_record_summaries.json
   def index
@@ -21,63 +22,4 @@ class DatabaseRecordSummariesController < ApplicationController
     end
   end
 
-  # GET /database_record_summaries/new
-  # GET /database_record_summaries/new.json
-  def new
-    @database_record_summary = DatabaseRecordSummary.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render :json => @database_record_summary }
-    end
-  end
-
-  # GET /database_record_summaries/1/edit
-  def edit
-    @database_record_summary = DatabaseRecordSummary.find(params[:id])
-  end
-
-  # POST /database_record_summaries
-  # POST /database_record_summaries.json
-  def create
-    @database_record_summary = DatabaseRecordSummary.new(params[:database_record_summary])
-
-    respond_to do |format|
-      if @database_record_summary.save
-        format.html { redirect_to @database_record_summary, :notice => 'Database record summary was successfully created.' }
-        format.json { render :json => @database_record_summary, :status => :created, :location => @database_record_summary }
-      else
-        format.html { render :action => "new" }
-        format.json { render :json => @database_record_summary.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /database_record_summaries/1
-  # PUT /database_record_summaries/1.json
-  def update
-    @database_record_summary = DatabaseRecordSummary.find(params[:id])
-
-    respond_to do |format|
-      if @database_record_summary.update_attributes(params[:database_record_summary])
-        format.html { redirect_to @database_record_summary, :notice => 'Database record summary was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render :action => "edit" }
-        format.json { render :json => @database_record_summary.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /database_record_summaries/1
-  # DELETE /database_record_summaries/1.json
-  def destroy
-    @database_record_summary = DatabaseRecordSummary.find(params[:id])
-    @database_record_summary.destroy
-
-    respond_to do |format|
-      format.html { redirect_to database_record_summaries_url }
-      format.json { head :no_content }
-    end
-  end
 end
---
+++ db/migrate/20140407091106_fix_database_record_summaries.rb
@@ -0,0 +1,29 @@
+class FixDatabaseRecordSummaries < ActiveRecord::Migration
+  def up
+    GridCpuRecord.connection.execute('
+      CREATE 
+    ALGORITHM = UNDEFINED 
+    DEFINER = `root`@`dgas-services.to.infn.it` 
+    SQL SECURITY DEFINER
+VIEW `database_record_summaries` AS
+    select
+    `database_records`.`id` AS `id`,
+        cast(`database_records`.`time` as date) AS `record_date`,
+        `database_tables`.`name` AS `table_name`,
+        `database_schemes`.`name` AS `scheme_name`,
+        sum(`database_records`.`rows`) AS `rows`,
+        sum(`database_records`.`tablesize`) AS `tablesize`,
+        sum(`database_records`.`indexsize`) AS `indexsize`,
+        `database_schemes`.`publisher_id` AS `publisher_id`,
+        `database_schemes`.`database_descr_id` AS `database_descr_id`
+    from
+        ((`database_records`
+        left join `database_tables` ON ((`database_records`.`database_table_id` = `database_tables`.`id`)))
+        left join `database_schemes` ON ((`database_tables`.`database_scheme_id` = `database_schemes`.`id`)))
+    group by `database_schemes`.`id` , `database_tables`.`id` , cast(`database_records`.`time` as date)
+    ')
+  end
+  def down
+    
+  end
+end
