eedea51223f55f3a4e238ab4cd2be93cc14597bf
Andrea Guarise <andrea.guarise@to.infn.it>
2014-06-17 16:49:10 +0200

    grid id view

--- app/controllers/cpu_grid_ids_controller.rb
+++ app/controllers/cpu_grid_ids_controller.rb
@@ -2,11 +2,12 @@ class CpuGridIdsController < ApplicationController
   # GET /cpu_grid_ids
   # GET /cpu_grid_ids.json
   def index
-    @cpu_grid_ids = CpuGridId.all
+    @cpu_grid_ids = CpuGridId.paginate(:page=>params[:page], :per_page => config.itemsPerPageHTML).all
 
     respond_to do |format|
       format.html # index.html.erb
       format.json { render :json => @cpu_grid_ids }
+      format.xml { render :xml => @cpu_grid_ids }
     end
   end
 
@@ -18,6 +19,7 @@ class CpuGridIdsController < ApplicationController
     respond_to do |format|
       format.html # show.html.erb
       format.json { render :json => @cpu_grid_id }
+      format.json { render :xml => @cpu_grid_id }
     end
   end
 end
