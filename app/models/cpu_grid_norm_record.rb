9fd6ea5762446f1289ea2af4c441ca17b3c38b33
Andrea Guarise <andrea.guarise@to.infn.it>
2014-06-19 13:51:21 +0200

    Altered view

--- app/models/cpu_grid_norm_record.rb
+++ app/models/cpu_grid_norm_record.rb
@@ -1,3 +1,3 @@
 class CpuGridNormRecord < ActiveRecord::Base
-  attr_accessible :FQAN, :benchmark_value_id, :cput, :execHost, :localUser, :lrmsId, :nodect, :nodes, :pmem, :publisher_id, :recordDate, :userDN, :vmem, :wallt
+  attr_accessible :FQAN, :benchmark_value_id, :cput, :execHost, :localUser, :lrmsId, :nodect, :nodes, :pmem, :publisher_id, :recordDate, :userDN, :vmem, :vo, :voGroup, :voRole, :wallt
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140619113112) do
+ActiveRecord::Schema.define(:version => 20140619143228) do
 
   create_table "batch_execute_records", :force => true do |t|
     t.string   "uniqueId"
@@ -179,6 +179,9 @@ ActiveRecord::Schema.define(:version => 20140619113112) do
     t.integer  "vmem"
     t.string   "userDN"
     t.string   "FQAN"
+    t.string   "vo"
+    t.string   "voGroup"
+    t.string   "voRole"
     t.integer  "benchmark_value_id", :limit => 8
   end
 
