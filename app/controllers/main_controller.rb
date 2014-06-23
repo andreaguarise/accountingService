f7432277991e64bcf2baff285a21a9ad63e57abb
Andrea Guarise <andrea.guarise@to.infn.it>
2014-06-23 09:18:23 +0200

    Imrpoved query model for main index

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,5 +5,6 @@ class MainController < ApplicationController
     @emiStorageBySite = EmiStorageRecord.group(:site).count #FIXME this should go through joins as for the other record types
     @blahBySite = Site.joins(:blah_records).group(:site_id).count
     @batchBySite = Site.joins(:batch_execute_records).group(:site_id).count
+    @cloudBySite = Site.joins(:cloud_records).group(:site_id).count
   end
 end
