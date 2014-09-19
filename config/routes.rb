f43518f4907cf02d51fcc8ee7dee1e09dc160e7c
Andrea Guarise <andrea.guarise@to.infn.it>
2014-09-19 14:00:41 +0200

    New pubic homepage

--- config/routes.rb
+++ config/routes.rb
@@ -130,6 +130,7 @@ AccountingService::Application.routes.draw do
 
 
   get "main/index"
+  get "main/list"
   
   
 
@@ -226,7 +227,7 @@ AccountingService::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-  root :to => 'main#list', :as => 'list'
+  root :to => 'main#list', :as => 'main'
 # See how all your routes lay out with "rake routes"
 
 # This is a legacy wild controller route that's not recommended for RESTful applications.
