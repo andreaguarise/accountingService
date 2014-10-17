0e71f5ef5c0cf104eac22311e18fec26261049e2
Andrea Guarise <andrea.guarise@to.infn.it>
2014-10-17 11:35:04 +0200

    Improved pledge graphite metric definition

--- lib/pledge2graphite_defs.rb
+++ lib/pledge2graphite_defs.rb
@@ -15,25 +15,26 @@ class Graphics < BaseGraph
 
   def defs
    t= Table.new(GridPledge,"validFrom",@options[:date])
-     result = t.result.joins(:site)
+     result = t.result.joins(:site,:benchmark_type)
      result = result.select("
           `sites`.`name` as siteName,
+          `benchmark_types`.`name` as benchmark,
           validFrom,
           validTo,
          value")
       result = result.group("siteName")
     result.each do |r|
-      puts "#{r['siteName']} - date: #{"#{r['validFrom']}".to_datetime} #{r['validTo'].to_datetime}  pledged: #{r['value']}"
+      puts "#{r['siteName']} - date: #{"#{r['validFrom']}".to_datetime} #{r['validTo'].to_datetime}  pledged: #{r['value']} #{r['benchmark']}"
       if !@options[:dryrun]
           d1=r['validFrom'].to_date
           d2=r['validTo'].to_date
                 metrs = {
-                "faust_pledged_test.pledged.by_site.#{r['siteName']}.pledged" => r['value'].to_f
+                "faust_pledge.by_site.#{r['siteName']}.#{r['benchmark']}" => r['value'].to_f
                 }
           (d1..d2).map{ |d|
-                        t=d.to_time
+                t=d.to_time
                 metrs = {
-                        "faust_pledged_test.pledged.by_site.#{r['siteName']}.pledged" => r['value'].to_f
+                "faust_pledge.by_site.#{r['siteName']}.#{r['benchmark']}" => r['value'].to_f
                 }
                 #client = GraphiteAPI.new( :graphite => @options[:graphiteUrl] )
                 puts "#{r['siteName']} - date: #{t.to_datetime}"
