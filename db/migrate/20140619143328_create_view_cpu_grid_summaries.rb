b5bd1652f9a427e75e0db9f8e71e5dc2ba4c48e6
Andrea Guarise <andrea.guarise@to.infn.it>
2014-11-04 14:39:33 +0100

    Fixing db migrations

--- db/migrate/20140619143328_create_view_cpu_grid_summaries.rb
+++ db/migrate/20140619143328_create_view_cpu_grid_summaries.rb
@@ -1,23 +1,23 @@
 class CreateViewCpuGridSummaries < ActiveRecord::Migration
   def up
     CpuGridSummary.connection.execute('
-    CREATE VIEW `cpu_grid_summaries` AS
+    VIEW `cpu_grid_summaries` AS
     select 
         `cpu_grid_norm_records`.`id` AS `id`,
         cast(`cpu_grid_norm_records`.`recordDate` as date) AS `date`,
         `cpu_grid_norm_records`.`publisher_id` AS `publisher_id`,
-        `cpu_grid_norm_records`.`userDN` AS `userDN`,
+        `cpu_grid_norm_records`.`globalUserName` AS `globalUserName`,
         `cpu_grid_norm_records`.`vo` AS `vo`,
-        `cpu_grid_norm_records`.`FQAN` AS `FQAN`,
+        `cpu_grid_norm_records`.`fqan` AS `fqan`,
         count(`cpu_grid_norm_records`.`id`) AS `records`,
-        sum(`cpu_grid_norm_records`.`cput`) AS `cput`,
-        sum(`cpu_grid_norm_records`.`wallt`) AS `wallt`,
+        sum(`cpu_grid_norm_records`.`cpuDuration`) AS `cpuDuration`,
+        sum(`cpu_grid_norm_records`.`wallDuration`) AS `wallDuration`,
         avg(`benchmark_values`.`value`) AS `benchmark_value`,
         `benchmark_values`.`benchmark_type_id` AS `benchmark_type_id`
     from
         (`cpu_grid_norm_records`
         left join `benchmark_values` ON ((`benchmark_values`.`id` = `cpu_grid_norm_records`.`benchmark_value_id`)))
-    group by cast(`cpu_grid_norm_records`.`recordDate` as date) , `cpu_grid_norm_records`.`publisher_id` , `cpu_grid_norm_records`.`userDN` , `cpu_grid_norm_records`.`vo` , `cpu_grid_norm_records`.`FQAN` , `benchmark_values`.`benchmark_type_id`
+    group by cast(`cpu_grid_norm_records`.`recordDate` as date) , `cpu_grid_norm_records`.`publisher_id` , `cpu_grid_norm_records`.`globalUserName` , `cpu_grid_norm_records`.`vo` , `cpu_grid_norm_records`.`fqan` , `benchmark_values`.`benchmark_type_id`
     ')
   end
   
