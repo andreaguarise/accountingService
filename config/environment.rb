dc1357b8122aa5cc805b8dd18549bfe4883bd8a6
Andrea Guarise <andrea.guarise@to.infn.it>
2014-11-28 09:14:29 +0100

    Adding link and config parameters for amq broker

--- config/environment.rb
+++ config/environment.rb
@@ -11,6 +11,8 @@ AccountingService::Application.configure do
   config.graphiteUrl = "http://localhost:80"
   config.graphiteGraphOpts = "&height=250&width=600"
   config.grafanaUrl = "http://dgas-dev-24.to.infn.it:8081"
+  config.brokerInterface = "http://changeme:8161"
+  config.brokerStomp = "changeme:61613"
   config.modAuthSecretFile = "/etc/faust/auth_tkt.conf"
   config.warningTimeInterval = 129600
 end
