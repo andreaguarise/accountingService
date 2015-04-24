1bc6bdc37a2dc6d3332e33648b44a3180c56900b
Andrea Guarise <aguarise@gmail.com>
2015-04-24 02:52:32 -0400

    removed unused files

--- lib/tasks/apelMonthlyReport.rb
+++ lib/tasks/apelMonthlyReport.rb
@@ -17,10 +17,10 @@ class Table
   def result
     result = @obj
     if @stopDate != ""
-      result = result.where("`cpu_grid_norm_records`.`#{timeField}`<'#{@stopDate}'") 
+      result = result.where("#{timeField}<'#{@stopDate}'") 
     end 
     if @startDate != ""
-      result = result.where("`cpu_grid_norm_records`.`#{timeField}`>='#{@startDate}'") 
+      result = result.where("#{timeField}>='#{@startDate}'") 
     end 
     if @site != ""
       result = result.where("sites.name = '#{@site}'")
@@ -50,29 +50,150 @@ class BaseGraph
 end
 
 class ELData < BaseGraph
+  
+  def print record
+    content = "siteName  Year  Month  vo  count  wall_H  wall_H_ksi2k  \\n"
+    record.each do |r| 
+      content += "#{r['siteName']}   #{r['year']}  #{r['month']}  #{r['vo']}  #{r['count']}  #{r['wall_H'].round(2)} #{r['wall_H_ksi2k'].round(2)}  \\n"
+    end
+    navigation = ""
+    puts '{
+  "rows": [
+    {
+      "title": "Summary",
+      "height": "70px",
+      "editable": false,
+      "collapse": false,
+      "collapsable": true,
+      "panels": [
+        {
+          "error": false,
+          "span": 5,
+          "editable": false,
+          "type": "text",
+          "mode": "markdown",
+          "content": "' + navigation + '"
+      ,"style": {},
+          "title": "Navigation"
+        }
+      ],
+      "notice": false
+    },
+    {
+      "title": "Statistics",
+      "height": "800px",
+      "editable": true,
+      "collapse": false,
+      "collapsable": true,
+      "panels": [
+        {
+          "error": false,
+          "span": 12,
+          "editable": true,
+          "type": "text",
+          "mode": "markdown",
+          "content": "' + content + '"
+      ,"style": {},
+          "title": "Monthly report"
+        }
+      ],
+      "notice": false
+    }
+  ],
+  "services": {
+    "filter": {
+      "time": {
+        "from": "now-30d",
+        "to": "now"
+      }
+    }
+  },
+  "title": "Summary",
+  "editable": "false",
+  "style": "light",
+  "panel_hints": "false",
+  "loader": {
+    "save_gist": false,
+    "save_elasticsearch": true,
+    "save_local": true,
+    "save_default": false,
+    "save_temp": false,
+    "save_temp_ttl_enable": false,
+    "save_temp_ttl": "30d",
+    "load_gist": false,
+    "load_elasticsearch": true,
+    "load_elasticsearch_size": 20,
+    "load_local": true,
+    "hide": true
+  },
+  "tags": [],
+  "timezone": "browser",
+  "failover": false,
+  "pulldowns": [
+    {
+      "type": "templating"
+    },
+    {
+      "type": "annotations"
+    },
+    {
+      "type": "filtering",
+      "enable": false
+    }
+  ],
+  "nav": [
+    {
+      "type": "timepicker",
+      "collapse": false,
+      "notice": false,
+      "enable": true,
+      "status": "Stable",
+      "time_options": [
+        "12h",
+        "24h",
+        "2d",
+        "7d",
+        "30d"
+      ],
+      "refresh_intervals": [
+        "5m",
+        "15m",
+        "30m",
+        "1h",
+        "2h",
+        "1d"
+      ],
+      "now": true
+    }
+  ],
+  "refresh": false
+}'
+  end
 
   def defs
-    t= Table.new(CpuGridNormRecord,"recordDate",@options[:fromDate],@options[:toDate],@options[:site])
+    t= Table.new(CpuGridNormRecord,"FROM_UNIXTIME(endTime)",@options[:fromDate],@options[:toDate],@options[:site])
       result = t.result.joins(:publisher => [:resource => :site])
       result = result.joins(:benchmark_value)
       result = result.select(
-	  ["year(recordDate) as year",
-	    "month(recordDate) as month",
+	  ["year(FROM_UNIXTIME(endTime)) as year",
+	    "month(FROM_UNIXTIME(endTime)) as month",
        "`sites`.`name` as siteName",
 	     "vo",
-       "sum(wallDuration)/3600", 
+       "sum(wallDuration)/3600 as wall_H", 
        "count(*) as count",
-       "(benchmark_values.value*wallDuration)/3600000 as wall_H_ksi2k"
+       "sum(benchmark_values.value*wallDuration)/3600000 as wall_H_ksi2k"
 	])
-      index=1
+      result = result.group("siteName,year,month,vo")
+      result = result.order("siteName,year,month,vo")
       time0 = Time.now.to_i
       if @options[:verbose]
         puts result.to_sql
       end
-      result.each do |r|
-       	  puts "#{index} -- #{r.to_json}" 
-	       index = index +1
-      end
+      print result
+      #result.each do |r|
+       #	  puts "#{index} -- #{r.to_json}" 
+	    #   index = index +1
+      #end
   end
   
 end
